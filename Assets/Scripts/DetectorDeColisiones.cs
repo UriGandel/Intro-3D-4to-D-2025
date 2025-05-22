fbfbafe0a91073945dafac926e9cbe0880eb87f5
Uriel Gandelman Rothschild <73185410+UriGandel@users.noreply.github.com>
2025-05-22 10:25:24 -0300

    Detector de colisiones

---
+++ Assets/Scripts/DetectorDeColisiones.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectorDeColisiones : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Contacto");
+        Destroy(gameObject);
+    }
+}
