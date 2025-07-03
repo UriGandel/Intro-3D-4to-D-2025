ed2d01473f4ea393ed6bbc8403382d999a821305
Uriel Gandelman Rothschild <73185410+UriGandel@users.noreply.github.com>
2025-07-03 10:42:23 -0300

    .

---
+++ Assets/Scripts/InteractionArea.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractionArea : MonoBehaviour
+{
+    public GameObject UIinteractionMessage;
+    // Start is called before the first frame update
+    void Start()
+    {
+        UIinteractionMessage.SetActive(false);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        MercaderiaScript mercaderia = other.GetComponent<MercaderiaScript>();
+        if (mercaderia) {
+                    UIinteractionMessage.SetActive(true);
+
+
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        MercaderiaScript mercaderia = other.GetComponent<MercaderiaScript>();
+
+            UIinteractionMessage.SetActive(false);
+            mercaderia = null;
+    }
+}
